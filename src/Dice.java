de80ed01148483a231a1bc0439c0052de1878c96
Masaki Sakugawa <e175728@ie.u-ryukyu.ac.jp>
2017-12-05 10:42:16 +0900

    ex3

---
+++ src/Dice.java
@@ -0,0 +1,20 @@
+package jp.ac.uryukyu.ie.ex3.pair01;
+
+public class Dice{
+    private int value;
+
+    public Dice(){
+        play();
+        this.value = value;
+    }
+
+    public int getValue(){ return value;}
+
+    public void setValue(int value){
+        this.value = value;
+    }
+
+    public void play(){
+        value = (int)(Math.random()*6) + 1;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import jp.ac.uryukyu.ie.ex3.pair01.Dice;
+public class Main {
+    public static void main(String[] args){
+	Dice dice = new Dice(); //オブジェクト生成(13.3節)
+	System.out.println(dice.getValue()); //diceオブジェクトの利用(13.4節)
+    }
+}
